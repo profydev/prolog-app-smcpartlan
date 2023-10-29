84a41fae7753bc72465749472983330341bc9420
smcpartlan <sammcpartlan@gmail.com>
2023-10-29 19:20:08 +0000

    Decoupled the request error module into it's own component from the Project page

--- features/issues/index.ts
+++ features/issues/index.ts
@@ -1,1 +1,2 @@
+export { useGetIssues } from "./api/use-get-issues";
 export * from "./components/issue-list";
---
+++ features/layout/request-error/request-error.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./request-error.module.scss";
+import { Button } from "../../ui/button";
+import { RequestErrorRetry } from "@api/error.types";
+
+interface RequestErrorProps {
+  retryMethod: RequestErrorRetry;
+}
+
+export function RequestError({ retryMethod }: RequestErrorProps) {
+  return (
+    <div className={styles.errorContainer} data-cy="error">
+      <img
+        className={styles.alertIcon}
+        src="/icons/alert-circle.svg"
+        alt="Error"
+      />
+      <div className={styles.errorContent}>
+        <div className={styles.errorText}>
+          There was a problem while loading the project data
+        </div>
+        <div className={styles.actionContainter}>
+          <Button
+            className={styles.tryAgainButton}
+            onClick={retryMethod}
+            data-cy="retry"
+          >
+            <div className={styles.tryAgainButtonBase}>
+              <div className={styles.buttonText}>Try Again</div>
+              <img
+                className={styles.buttonIcon}
+                src="/icons/red-arrow-right.svg"
+                alt="Try Again"
+              />
+            </div>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
